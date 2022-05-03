d9eb2b3b73070dede2621dee75c171b7e8e21564
Perepechko <very.very.serious.things@gmail.com>
2022-05-03 12:52:31 +0300

    two files

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+OID=pd.read_csv("zillow.csv")
+df=OID.groupby(["Beds"])
+figure, axes=plt.subplots(nrows=1,ncols=len(df.groups.keys()))
+for (key, ax)in zip(df.groups.keys(), axes.flatten()):
+    df.get_group(key).plot(ax=ax)
+    ax.legend()
+    ax.set_title(key)
+plt.show()
+
+#jL7f3BihZ@4f
